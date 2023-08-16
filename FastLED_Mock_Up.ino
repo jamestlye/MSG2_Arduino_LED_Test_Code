73071e768bec9f19ed43eb8b58f2fc2be56a0baf
James Ye <80552984+jamestlye@users.noreply.github.com>
2023-08-16 11:53:37 -0400

    Update FastLED_Mock_Up.ino

--- FastLED_Mock_Up.ino
+++ FastLED_Mock_Up.ino
@@ -1,13 +1,16 @@
 #include <FastLED.h>
 
 #define SERIAL_SPEED 9600
-#define TIME_INTERVAL 1500
-#define LOCAL_INTERVAL 300
+#define TIME_INTERVAL 0
+#define LOCAL_INTERVAL 2400
+#define FLASH_INTERVAL 300
 #define OPTICAL_PIN 4
 #define LED_PIN_1 2
 #define LED_PIN_2 3
 #define NUM_STRIPS 2
-#define NUM_LEDS_PER_STRIP 106
+#define NUM_LEDS_PER_STRIP 94
+#define DIM_TRIGGER "1"
+#define BRIGHT_TRIGGER "2"
 
 bool walkThrough = false;
 unsigned long currentTime = millis();
@@ -23,6 +26,7 @@ void setup() {
   FastLED.addLeds<WS2813, LED_PIN_1, GRB>(leds[0], NUM_LEDS_PER_STRIP);
   FastLED.addLeds<WS2813, LED_PIN_2, GRB>(leds[1], NUM_LEDS_PER_STRIP);
   FastLED.clear(true);
+  randomSeed(analogRead(0));
 } 
 
 void loop() {
@@ -32,6 +36,13 @@ void loop() {
     processing();
     Serial.write(walkThrough);
   }
+
+  if(Serial.read() == DIM_TRIGGER){
+    FastLED.setBrightness(0);
+  } else if (Serial.read() == BRIGHT_TRIGGER){
+    FastLED.setBrightness(255);
+  }
+
   idle(); //default green animation
 }
 
@@ -43,22 +54,22 @@ void idle(){
 }
 
 void localization(){
-  randNum = random(0,3); //randomly select which area, top, middle, or bottom, to localize
-  for(int x = 0; x < NUM_STRIPS; x++) {
-    fill_solid(leds[x]+(randNum*35), 35, CRGB::Red);
-    FastLED.show();
+  // randNum = random(0,3); //randomly select which area, top, middle, or bottom, to localize
+  randNum = random(0,3);
+  currentTime = previousTime = millis(); 
+  while ((currentTime - previousTime)< LOCAL_INTERVAL){
+    for(int x = 0; x < NUM_STRIPS; x++) {
+      fill_solid(leds[x]+(randNum*(NUM_LEDS_PER_STRIP/3)), (NUM_LEDS_PER_STRIP/3), CRGB::Red);
+      FastLED.show();
+    }
+    delay(FLASH_INTERVAL);
+    for(int x = 0; x < NUM_STRIPS; x++) {
+      fill_solid(leds[x], NUM_LEDS_PER_STRIP, CRGB::Yellow);
+      FastLED.show();
+    }
+    delay(FLASH_INTERVAL);
+    currentTime = millis();
   }
-  delay(LOCAL_INTERVAL);
-  for(int x = 0; x < NUM_STRIPS; x++) {
-    fill_solid(leds[x], NUM_LEDS_PER_STRIP, CRGB::Yellow);
-    FastLED.show();
-  }
-  delay(LOCAL_INTERVAL);
-  for(int x = 0; x < NUM_STRIPS; x++) {
-    fill_solid(leds[x]+(randNum*35), 35, CRGB::Red);
-    FastLED.show();
-  }
-  delay(LOCAL_INTERVAL);
   walkThrough = false; //done one walkthrough!
 }
 
